b5c29b3e7a008182aa9a561c0bf9136ed3dfeb63
asgar <sahp004@gmail.com>
2020-12-06 13:02:33 -0600

    DependencyInjection-Setter Injection Singleton

--- src/main/java/university/StudentDetails/StudentDetailsApplication.java
+++ src/main/java/university/StudentDetails/StudentDetailsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
 public class StudentDetailsApplication {
-
+//
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context= SpringApplication.run(StudentDetailsApplication.class, args);
 

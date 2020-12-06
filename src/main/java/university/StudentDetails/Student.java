fa60fed14a34f2a1d2fad94d579bfa97d683a673
asgar <sahp004@gmail.com>
2020-12-06 16:17:41 -0600

    ConstructorInjection @Autowired

--- src/main/java/university/StudentDetails/Student.java
+++ src/main/java/university/StudentDetails/Student.java
@@ -1,34 +1,62 @@
 package university.StudentDetails;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class Student {
 
     private String StudentName;
     private String rollNo;
+    private Address address;
     public String getRollNo() {
         return rollNo;
     }
-    @Value( "${student.rollno}" )
-    public void setRollNo(String rollNo) {
-        this.rollNo = rollNo;
+    //String StudentName,String rollNo,
+    public Student()
+    {
+        System.out.println("*******Student Constructor******");
+    }
+
+    public Student(String StudentName){
+        this.StudentName=StudentName;
+        // this.rollNo=rollNo;
+        //this.address=address;
+    }
+
+    @Autowired
+    public Student(Address address){
+        //this.StudentName=StudentName;
+       // this.rollNo=rollNo;
+        System.out.println("from autowire student(Adress) constructor");
+        this.address=address;
     }
 
 
     public String getStudentName() {
         return StudentName;
     }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    @Value( "${student.rollno}" )
+    public void setRollNo(String rollNo) {
+        this.rollNo = rollNo;
+    }
     @Value( "${student.name}" )
     public void setStudentName(String studentName) {
         StudentName = studentName;
     }
 
-
-
-    public Student()
-    {
-        System.out.println("*******Student Constructor******");
+    //@Autowired
+    public void setAddress(Address address) {
+        System.out.println("From Autowired SetAdress()");
+        this.address = address;
     }
+
+
 }
--- src/main/java/university/StudentDetails/StudentDetailsApplication.java
+++ src/main/java/university/StudentDetails/StudentDetailsApplication.java
@@ -1,13 +1,17 @@
 package university.StudentDetails;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @ComponentScan
+@Configuration
 public class StudentDetailsApplication {
-
+	//static ConfigurableApplicationContext context=SpringApplication.run(StudentDetailsApplication.class, args);
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context= SpringApplication.run(StudentDetailsApplication.class, args);
 
@@ -15,26 +19,16 @@ public class StudentDetailsApplication {
 
 		Student s2=context.getBean("student",Student.class);
 
-		System.out.println("@component scan Values	"+s2.getRollNo()+"		"+s2.getStudentName());
+		System.out.println("@component scan Values	"+s2.getRollNo()+"		"+s2.getStudentName()+"		"+s2.getAddress());
 
 
-		Student s1=context.getBean("studentBean",Student.class);
-		System.out.println("@Bean Annotation Values	 "+s1.getRollNo()+"		"+s1.getStudentName());
-
-
-
+	   //Student s1=context.getBean("studentBean",Student.class);
+		//System.out.println("@Bean Annotation Values	 "+s1.getRollNo()+"		"+s1.getStudentName()+"		"+s1.getAddress());
 
 
 
-	}
 
-	@Bean
-	public Student studentBean(){
-		System.out.print("From Bean Annotation");
-		Student s11=new Student();
-		s11.setRollNo("1");
-		s11.setStudentName("asg");
-		return s11;
+
 
 	}
 
@@ -44,4 +38,56 @@ public class StudentDetailsApplication {
 		return new Address();
 	}
 
+	@Bean
+	//@Autowired
+	public Student studentBean(){
+		System.out.print("From Bean Annotation");
+//		Student s11=new Student();
+//		s11.setRollNo("1");
+//		s11.setStudentName("asg");
+//
+		return new Student();
+
+	}
+
+
+
 }
+
+
+
+/*For @Componentscan first all component object got created if there is a dependency  then IOC will check if
+depency object is available and @autowired annotation available on both constructor and setter then both got
+injected constructor--->setter.
+IF the dependency class contain both Reference type and primitive type then primitive type usually get injected
+@Value annotation on setter() as constructor doesnt support @Value annotation.
+
+Conclusion: For Primitive Type (@Component)
+@value
+Setters()
+
+For Reference Type:  (@Component)
+@Autowired
+Constructor()
+
+@Autoowired
+@Setters()
+
+ */
+
+/*
+For @Bean Annotation
+Spring IOC will create objects, if there are dependecies then Dependency Class contains two constructor
+and if we are creating object using no arg constructor and @autowire is on parametrised constructor it will not
+get injected in order to do injection we need Setter() @Autowired.
+
+
+Constructor1();(Executed)
+
+@Autowired
+Constructor2();(Not Executed)
+
+@Autowired
+Setter();(Executed)
+
+ */
